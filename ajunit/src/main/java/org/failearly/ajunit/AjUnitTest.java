3945fd7de6270a757a5f300b552e827c12a4d3d1
Marko Umek <umek.marko@gmail.com>
2014-01-23 19:38:51 +0100

    AjUnitTest GIT repair.

--- ajunit/src/main/java/org/failearly/ajunit/AjUnitTest.java
+++ ajunit/src/main/java/org/failearly/ajunit/AjUnitTest.java
@@ -18,8 +18,45 @@
  */
 package org.failearly.ajunit;
 
+import org.failearly.ajunit.builder.JoinPointSelectorBuilder;
+
 /**
- * AjUnitTest is responsible for ...
+ * AjUnitTest the INTERFACE any ajUnit test must provide.
  */
 public interface AjUnitTest {
+
+    /**
+     * Setup the ajUnit test - The arrange/given part of ajUnit tests. Defines among other things the ajUnit Universe.
+     *
+     * @param ajUnitSetup the setup instance.
+     */
+    void setup(AjUnitSetup ajUnitSetup);
+
+    /**
+     * Within this method the test fixture classes should be called, so that the aspect could be applied - the act/when part of ajUnit tests.
+     * <br/></br>
+     * Important notes:<br/></br>
+     * <ul>
+     *     <li>Execute methods/constructors only on classes which has been added to the ajUnit universe.
+     *              (see {@link org.failearly.ajunit.AjUnitSetup#addTestFixtureClass(Class)}</li>
+     *     <li>Execute also parts of the test fixture classes which should not be selected by your Aspect's pointcut.</li>
+     * </ul>
+     */
+    void execute();
+
+    /**
+     * Assert the pointcut definition - the assert/then part of ajUnit tests. <b>This is the most important function of ajUnit.</b>
+     * <br/></br>
+     * The {@code joinPointSelectorBuilder} is responsible to build the <i>join point selector</i>. A join point selector
+     * is the counterpart of an Aspect's pointcut. It separates the ajUnit Universe in two halves:<br/></br>
+     * <ul>
+     *     <li>The join points which should be applied (or touched) by your specified Aspect. This is the base for the <i>false positive</i> test.
+     *     </li>
+     *     <li>And the join points which shouldn't be touched at all. This is the base for the <i>true negative<i/> test</li>
+     * </ul>
+     *
+     * @param joinPointSelectorBuilder the join point selector builder.
+     */
+    void assertPointcut(JoinPointSelectorBuilder joinPointSelectorBuilder);
+
 }
